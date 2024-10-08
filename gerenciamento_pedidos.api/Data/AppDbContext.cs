56751caa55ff0e2f3d92fd1a9730d51ada8ed14c
joao <joaoguilhermesantos2909@gmail.com>
2024-10-08 17:54:41 -0300

    Add project files.

---
+++ gerenciamento_pedidos.api/Data/AppDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace gerenciamento_pedidos.api.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> opts) : base(opts) {}
+}
